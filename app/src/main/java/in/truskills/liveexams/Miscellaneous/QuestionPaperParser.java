9fc7beeba812604e4402bd6aa60790490b89b01a
Shivangi Gupta <shivangilm10@gmail.com>
2017-02-01 13:56:48 +0530

    QuestionPaperParser complete Web view content complete

--- app/src/main/java/in/truskills/liveexams/Miscellaneous/QuestionPaperParser.java
+++ app/src/main/java/in/truskills/liveexams/Miscellaneous/QuestionPaperParser.java
@@ -1,5 +1,7 @@
 package in.truskills.liveexams.Miscellaneous;
 
+import android.graphics.Path;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -42,8 +44,27 @@ public class QuestionPaperParser {
     private static String Sections="Sections";
     private static String Attributes="Attributes";
     private static String Section="Section";
-
-
+    private static String SectionMaxMarks="SectionMaxMarks";
+    private static String SectionTime="SectionTime";
+    private static String SectionDescription="SectionDescription";
+    private static String SectionRules="SectionRules";
+    private static String SectionQuestions="SectionQuestions";
+    private static String Question="Question";
+    private static String AskedIn="AskedIn";
+    private static String Language="Language";
+    private static String CorrectAnswer="CorrectAnswer";
+    private static String QuestionCorrectMarks="QuestionCorrectMarks";
+    private static String QuestionIncorrectMarks="QuestionIncorrectMarks";
+    private static String PassageID="PassageID";
+    private static String QuestionType="QuestionType";
+    private static String QuestionTime="QuestionTime";
+    private static String QuestionDifficultyLevel="QuestionDifficultyLevel";
+    private static String QuestionRelativeTopic="QuestionRelativeTopic";
+    private static String Year="Year";
+    private static String QuestionText="QuestionText";
+    private static String Options="Options";
+    private static String Option="Option";
+    private static String _="_";
 
     public static HashMap<String,String> resultParser(String result) throws JSONException {
         HashMap<String,String> mapper=new HashMap<>();
@@ -131,5 +152,210 @@ public class QuestionPaperParser {
         mapper.put("Section",jsonObject.getJSONArray(Section).toString());
         return mapper;
     }
+
+    public static HashMap<String,ArrayList<String>> SectionParser(String mySection) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        JSONArray jsonArray=new JSONArray(mySection);
+        int length=jsonArray.length();
+        ArrayList<String> SectionMaxMarksList=new ArrayList<>();
+        ArrayList<String> SectionTimeList=new ArrayList<>();
+        ArrayList<String> SectionDescriptionList=new ArrayList<>();
+        ArrayList<String> SectionRulesList=new ArrayList<>();
+        ArrayList<String> SectionQuestionsList=new ArrayList<>();
+        ArrayList<String> AttributesList=new ArrayList<>();
+
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=jsonArray.getJSONObject(i);
+            SectionMaxMarksList.add(jsonObject.getJSONArray(SectionMaxMarks).get(0).toString());
+            SectionTimeList.add(jsonObject.getJSONArray(SectionTime).get(0).toString());
+            SectionDescriptionList.add(jsonObject.getJSONArray(SectionDescription).get(0).toString());
+            SectionRulesList.add(jsonObject.getJSONArray(SectionRules).get(0).toString());
+            SectionQuestionsList.add(jsonObject.getJSONArray(SectionQuestions).getJSONObject(0).toString());
+            AttributesList.add(jsonObject.getJSONObject(Attributes).toString());
+        }
+
+        mapper.put("SectionMaxMarks",SectionMaxMarksList);
+        mapper.put("SectionTime",SectionTimeList);
+        mapper.put("SectionDescription",SectionDescriptionList);
+        mapper.put("SectionRules",SectionRulesList);
+        mapper.put("SectionQuestions",SectionQuestionsList);
+        mapper.put("Attributes",AttributesList);
+        return mapper;
+    }
+
+    public static HashMap<String,ArrayList<String>> getAttributesOfSection(ArrayList<String> myAttributesList) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        int length=myAttributesList.size();
+        ArrayList<String> idList=new ArrayList<>();
+        ArrayList<String> NameList=new ArrayList<>();
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=new JSONObject(myAttributesList.get(i));
+            idList.add(jsonObject.getString(id));
+            NameList.add(jsonObject.getString(Name));
+        }
+        mapper.put("id",idList);
+        mapper.put("Name",NameList);
+        return mapper;
+    }
+
+    public static int getNoOfSections(String mySection) throws JSONException {
+        JSONArray jsonArray=new JSONArray(mySection);
+        int length=jsonArray.length();
+        return length;
+    }
+
+    public static HashMap<String,String> SectionQuestionsParser(String mySectionQuestions) throws JSONException {
+        HashMap<String,String> mapper=new HashMap<>();
+        JSONObject jsonObject=new JSONObject(mySectionQuestions);
+        mapper.put("Question",jsonObject.getJSONArray(Question).toString());
+        return mapper;
+    }
+
+    public static HashMap<String,ArrayList<String>> QuestionParser(String myQuestion) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        JSONArray jsonArray=new JSONArray(myQuestion);
+        int length=jsonArray.length();
+        ArrayList<String> AskedInList=new ArrayList<>();
+        ArrayList<String> LanguageList=new ArrayList<>();
+        ArrayList<String> AttributeList=new ArrayList<>();
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=jsonArray.getJSONObject(i);
+            AskedInList.add(jsonObject.getJSONArray(AskedIn).get(0).toString());
+            LanguageList.add(jsonObject.getJSONArray(Language).toString());
+            AttributeList.add(jsonObject.getJSONObject(Attributes).toString());
+        }
+
+        mapper.put("AskedIn",AskedInList);
+        mapper.put("Language",LanguageList);
+        mapper.put("Attributes",AttributeList);
+        return mapper;
+    }
+
+    public static int getNoOfQuestionInOneSection(String myQuestion) throws JSONException {
+        JSONArray jsonArray=new JSONArray(myQuestion);
+        int length=jsonArray.length();
+        return length;
+    }
+
+    public static HashMap<String,ArrayList<String>> getAttributesOfQuestion(ArrayList<String> myAttributesOfQuestion) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        int length=myAttributesOfQuestion.size();
+        ArrayList<String> idList=new ArrayList<>();
+        ArrayList<String> CorrectAnswerList=new ArrayList<>();
+        ArrayList<String> QuestionCorrectMarksList=new ArrayList<>();
+        ArrayList<String> QuestionIncorrectMarksList=new ArrayList<>();
+        ArrayList<String> PassageIDList=new ArrayList<>();
+        ArrayList<String> QuestionTypeList=new ArrayList<>();
+        ArrayList<String> QuestionTimeList=new ArrayList<>();
+        ArrayList<String> QuestionDifficultyLevelList=new ArrayList<>();
+        ArrayList<String> QuestionRelativeTopicList=new ArrayList<>();
+
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=new JSONObject(myAttributesOfQuestion.get(i));
+            idList.add(jsonObject.getString(id));
+            CorrectAnswerList.add(jsonObject.getString(CorrectAnswer));
+            QuestionCorrectMarksList.add(jsonObject.getString(QuestionCorrectMarks));
+            QuestionIncorrectMarksList.add(jsonObject.getString(QuestionIncorrectMarks));
+            PassageIDList.add(jsonObject.getString(PassageID));
+            QuestionTypeList.add(jsonObject.getString(QuestionType));
+            QuestionTimeList.add(jsonObject.getString(QuestionTime));
+            QuestionDifficultyLevelList.add(jsonObject.getString(QuestionDifficultyLevel));
+            QuestionRelativeTopicList.add(jsonObject.getString(QuestionRelativeTopic));
+        }
+        return mapper;
+    }
+
+    public static HashMap<String,ArrayList<String>>  AskedInParser(ArrayList<String> myAskedIn) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        ArrayList<String> ExamNameList=new ArrayList<>();
+        ArrayList<String> YearList=new ArrayList<>();
+        int length=myAskedIn.size();
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=new JSONObject(myAskedIn.get(i));
+            ExamNameList.add(jsonObject.getJSONArray(ExamName).toString());
+            YearList.add(jsonObject.getJSONArray(Year).toString());
+        }
+        mapper.put("ExamName",ExamNameList);
+        mapper.put("Year",YearList);
+        return mapper;
+    }
+
+    public static ArrayList<String> getExamNamesOfOneQuestion(ArrayList<String> myExamName) throws JSONException {
+        int length=myExamName.size();
+        ArrayList<String> ExamNameList=new ArrayList<>();
+        for(int i=0;i<length;++i){
+            JSONArray jsonArray=new JSONArray(myExamName.get(i));
+            ExamNameList.add(jsonArray.get(i).toString());
+        }
+        return ExamNameList;
+    }
+
+    public static ArrayList<String> getYearsOfOneQuestion(ArrayList<String> myYear) throws JSONException {
+        int length=myYear.size();
+        ArrayList<String> YearList=new ArrayList<>();
+        for(int i=0;i<length;++i){
+            JSONArray jsonArray=new JSONArray(myYear.get(i));
+            YearList.add(jsonArray.get(i).toString());
+        }
+        return YearList;
+    }
+
+    public static HashMap<String,ArrayList<String>> LanguageParser(String myLanguage) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        JSONArray jsonArray=new JSONArray(myLanguage);
+        ArrayList<String> QuestionTextList=new ArrayList<>();
+        ArrayList<String> OptionsList=new ArrayList<>();
+        ArrayList<String> AttributesList=new ArrayList<>();
+        int length=jsonArray.length();
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=new JSONObject(jsonArray.get(i).toString());
+            QuestionTextList.add(jsonObject.getJSONArray(QuestionText).get(0).toString());
+            AttributesList.add(jsonObject.getJSONObject(Attributes).toString());
+            OptionsList.add(jsonObject.getJSONArray(Options).get(0).toString());
+        }
+        mapper.put("QuestionText",QuestionTextList);
+        mapper.put("Attributes",AttributesList);
+        mapper.put("Options",OptionsList);
+        return mapper;
+    }
+
+    public static String getAttributesOfOneLanguageOfOneQuestion(String myAttribute) throws JSONException {
+        JSONObject jsonObject=new JSONObject(myAttribute);
+        String myName=jsonObject.getString(Name);
+        return myName;
+    }
+
+    public static HashMap<String,ArrayList<String>> OptionsParser(ArrayList<String> myOption) throws JSONException {
+        HashMap<String,ArrayList<String >> mapper=new HashMap<>();
+        int length=myOption.size();
+        ArrayList<String> OptionList=new ArrayList<>();
+        for(int i=0;i<length;++i){
+            JSONObject jsonObject=new JSONObject(myOption.get(i));
+            OptionList.add(jsonObject.getJSONArray(Option).toString());
+        }
+        mapper.put("Option",OptionList);
+        return mapper;
+    }
+
+    public HashMap<String,ArrayList<String>> OptionParser(String myOption) throws JSONException {
+        HashMap<String,ArrayList<String>> mapper=new HashMap<>();
+        ArrayList<String> OptionList = new ArrayList<>();
+        JSONArray jsonArray=new JSONArray(myOption);
+        int length=jsonArray.length();
+        for(int i=0;i<length;++i){
+            OptionList.add(jsonArray.getJSONObject(i).toString());
+        }
+        mapper.put("Option",OptionList);
+        return mapper;
+    }
+
+    public static HashMap<String,String> oneOptionParser(String myOption) throws JSONException {
+        HashMap<String,String > mapper=new HashMap<>();
+        JSONObject jsonObject=new JSONObject(myOption);
+        mapper.put("_",jsonObject.getString(_));
+        mapper.put("Attributes",jsonObject.getString(Attributes));
+        return mapper;
+    }
+
 
 }
--- app/src/main/java/in/truskills/liveexams/Quiz/WebViewContent.java
+++ app/src/main/java/in/truskills/liveexams/Quiz/WebViewContent.java
@@ -1,6 +1,10 @@
 package in.truskills.liveexams.Quiz;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Shivansh Gupta on 27-01-2017.
@@ -8,40 +12,97 @@ import java.util.ArrayList;
 
 public class WebViewContent {
 
-    public String contentGenerator(final String question, final ArrayList<String> optionsList,final ArrayList<String> imagesList){
+    public String contentGenerator(final String question, final ArrayList<String> optionsList){
 
         int optionsListSize=optionsList.size();
-        int imagesListSize=imagesList.size();
-        int size=5;
+
+        String formattedQuestion=format(question);
+        ArrayList<String> formattedOptions=new ArrayList<>();
+        for(int i=0;i<optionsListSize;++i){
+            String formattedOption=format(optionsList.get(i));
+            formattedOptions.add(formattedOption);
+        }
+        String x="";
+        for(int i=0;i<optionsListSize;++i){
+            x=x+"<input type=\"radio\" id=\""+i+"\" name=\"options\" value=\""+i+"\" onclick=\"ok.performClick(this.value);\" ><label for=\"value\">"+formattedOptions.get(i)+"</label></input><br>";
+        }
+
         String content="<html>\n" +
                 "                <body onload=\"addInput('options')\">\n" +
                 "                Question:\n" +
-                "                question<br>\n" +
+                                formattedQuestion+
+                "                  <br>\n" +
                 "                Options:<br>\n" +
                 "                <form>\n" +
-                "                <div id=\"options\"></div>\n" +
+                "                <div>"+x+"</div>\n" +
                 "                </form>\n" +
-                "                <script>\n" +
-                "                       document.getElementById('options').innerHTML =\"abc\";\n" +
-                "\t\t\t\t\t\tvar counter=0; var limit="+size+"; \n" +
-                "                        function addInput(divName){ \n" +
-                "\t\t\t\t\t\t\tconsole.log(counter+\" \"+limit);\n" +
-                "\t\t\t\t\t\t\t   while(counter<limit){\n" +
-                "\t\t\t\t\t\t\t\tvar newdiv = document.createElement(\"div\"); \n" +
-                "\t\t\t\t\t\t\t\tnewdiv.innerHTML = \"<input type='radio' onclick=\\\"ok.performClick(this.value);\\\" id='\"+counter+\"' value='\"+counter+\"'><label for='\"+counter+\"'> text <img src='https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg' height=20 width=20/></label></input>\"\n" +
-                "\t\t\t\t\t\t\t\tdocument.getElementById(divName).appendChild(newdiv); \n" +
-                "\t\t\t\t\t\t\t\tcounter++; \n" +
-                "\t\t\t\t\t\t\t} \n" +
-                "                        }; \n" +
-                "\t\t\t\t\t\n" +
-                "                </script>\n" +
                 "                end\n" +
                 "                </body>\n" +
                 "                </html>;";
 
-
         return content;
 
+    }
+
+    public String format(String str){
+        ArrayList<String> jpgSubStr=new ArrayList<>();
+        ArrayList<String> pngSubStr=new ArrayList<>();
+        String searchableString = str;
+        String myStr=str;
+        String jpgStartSubStr,pngStartSubStr,s;
+
+        Pattern jpgStartPattern=Pattern.compile("[a-zA-Z0-9_-]*.jpg");
+        Matcher jpgStartMatcher=jpgStartPattern.matcher(searchableString);
+        if(jpgStartMatcher.find()){
+            int jpgStart=jpgStartMatcher.start();
+            jpgStart++;
+            int jpgEnd=jpgStartMatcher.end();
+            s=searchableString.substring(jpgStart,jpgEnd);
+            jpgStartSubStr="<img src='"+s+"'>";
+            myStr=myStr.replaceFirst("[a-zA-Z0-9_-]*.jpg",jpgStartSubStr);
+        }
+
+        Pattern pngStartPattern=Pattern.compile("[a-zA-Z0-9_-]*.png");
+        Matcher pngStartMatcher=pngStartPattern.matcher(searchableString);
+        if(pngStartMatcher.find()){
+            int pngStart=pngStartMatcher.start();
+            pngStart++;
+            int pngEnd=pngStartMatcher.end();
+            s=searchableString.substring(pngStart,pngEnd);
+            pngStartSubStr="<img src='"+s+"'>";
+            myStr=myStr.replaceFirst("[a-zA-Z0-9_-]*.png",pngStartSubStr);
+        }
+
+        Pattern jpgPattern=Pattern.compile("[ ][a-zA-Z0-9_-]*.jpg");
+        Matcher jpgMatcher=jpgPattern.matcher(searchableString);
+        while(jpgMatcher.find()){
+            int start=jpgMatcher.start();
+            start++;
+            int end=jpgMatcher.end();
+            s=searchableString.substring(start,end);
+            jpgSubStr.add("<img src='"+s+"'>");
+        }
+        Pattern pngPattern=Pattern.compile("[ ][a-zA-Z0-9_-]*.png");
+        Matcher pngMatcher=pngPattern.matcher(searchableString);
+        while(pngMatcher.find()){
+            int start=pngMatcher.start();
+            start++;
+            int end=pngMatcher.end();
+            s=searchableString.substring(start,end);
+            pngSubStr.add("<img src='"+s+"'>");
+        }
+
+        for(int i=0;i<jpgSubStr.size();++i){
+            myStr=myStr.replaceFirst("[ ][a-zA-Z0-9_-]*.jpg",jpgSubStr.get(i));
+        }
+
+        for(int i=0;i<pngSubStr.size();++i){
+            myStr=myStr.replaceFirst("[ ][a-zA-Z0-9_-]*.png",pngSubStr.get(i));
+        }
+
+        Log.d("result","Final:"+myStr);
+
+        return myStr;
     }
 
 }
